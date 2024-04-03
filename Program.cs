4511abad3819d3b8e8ffd9db9e334d32a66544d3
Anna Korzhenko <akorzhenko@kse.org.ua>
2024-04-03 20:49:32 +0300

    preparation part

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Please, enter some words > ");
+var userInput = Console.ReadLine();
+var userWords = userInput.Split();
+var badWords = new List<string>();
+var correctWords = File.ReadAllLines("../../../words_list.txt");
+
+foreach (var word in userWords)
+{
+    Console.WriteLine(word);
+    var isFound = false;
+    foreach (var correctWord in correctWords)
+    {
+        if (correctWord == word)
+        {
+            Console.WriteLine($"[OK] {word} found!");
+            isFound = true;
+            break;
+        }
+    }
+
+    if (isFound == false)
+    {
+        Console.WriteLine($"[Error] {word} not found!");
+        badWords.Add(word);
+    }
+}
+
+Console.Write("Looks like you have typos in those words: ");
+foreach (var badWord in badWords)
+{
+    Console.Write(badWord);
+    Console.Write(" ");
+}
+////// this is preparation
